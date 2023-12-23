3b5012b30a1276f12e5de1e37d47664926c3269e
EldarMemetov <eldarm9347@gmail.com>
2023-12-23 11:03:15 +0100

    yes

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, "0")}`;
+}
+
+const changeColorButton = document.querySelector(".button-started");
+console.log(changeColorButton);
+const body = document.body;
+
+changeColorButton.addEventListener("click", () => {
+  const newColor = getRandomHexColor();
+  body.style.backgroundColor = newColor;
+});
